4c7c0f3aaeedd812d2869aa9d8546d892e756416
Yaw <baffourampadu06@gmail.com>
2023-10-10 09:45:40 -0400

    Add all changes

--- TalkingPets/Zoo.java
+++ TalkingPets/Zoo.java
@@ -14,36 +14,63 @@ public class Zoo {
     public static void main(String[] args) {
 
         Scanner scanner = new Scanner((System.in));
-        ArrayList<Pets> pets = new ArrayList<>();
 
-        while (true){
 
-            System.out.println("How many pets do you have? or enter 0 to exit");
 
-            int numberOfPets = scanner.nextInt();
-            if (numberOfPets == 0) {
-                break;
-            }
-            scanner.nextLine();
-            System.out.println("What kind of pets are they");
-            System.out.println("1. Dog, 2. Cat, 3. Lion");
-            String typeOfPet = scanner.nextLine();
-            System.out.println("What is the name of the pet?");
-            String name = scanner.nextLine();
+        System.out.println("How many pets do you have?");
+        int numberOfPets = scanner.nextInt();
+        scanner.nextLine();
 
-            if (typeOfPet.equals("1")){
-                pets.add(new Dog(name));
-            } else if (typeOfPet.equals(("2"))){
-                pets.add(new Cat(name));
-            } else if (typeOfPet.equals(("3"))){
-                pets.add(new Lion(name));
-            } else {
-                System.out.println("Try again");
-            }
+        System.out.println("What are the names of the pets?");
+        String[] namesOfPets = new String[numberOfPets];
+        String[] typeOfPets =  new String[numberOfPets];
 
+        for (int i = 0; i < numberOfPets; i++) {
+            System.out.println("Enter the name of pet " + (i+1));
+            namesOfPets[i] = scanner.nextLine();
+            System.out.println("Enter the type of pet " + (i+1));
+            typeOfPets[i] = scanner.nextLine();
         }
 
-        System.out.println(pets);
+        System.out.println("What the pets say: ");
+        for (int i = 0; i < numberOfPets; i++) {
+            Pets pet = createPet(typeOfPets[i], namesOfPets[i]);
+            System.out.println(pet);
+            pet.speak();
+        }
+        
+
+
+//        ArrayList<Pets> pets = new ArrayList<>();
+//
+//        while (true){
+//
+//            System.out.println("How many pets do you have? or enter 0 to exit");
+//
+//            int numberOfPets = scanner.nextInt();
+//            if (numberOfPets == 0) {
+//                break;
+//            }
+//            scanner.nextLine();
+//            System.out.println("What kind of pets are they");
+//            System.out.println("1. Dog, 2. Cat, 3. Lion");
+//            String typeOfPet = scanner.nextLine();
+//            System.out.println("What is the name of the pet?");
+//            String name = scanner.nextLine();
+//
+//            if (typeOfPet.equals("1")){
+//                pets.add(new Dog(name));
+//            } else if (typeOfPet.equals(("2"))){
+//                pets.add(new Cat(name));
+//            } else if (typeOfPet.equals(("3"))){
+//                pets.add(new Lion(name));
+//            } else {
+//                System.out.println("Try again");
+//            }
+
+    //    }
+
+//        System.out.println(pets);
 
 
 
@@ -76,6 +103,11 @@ public class Zoo {
 //                System.out.println("Here is your food");
 //            }
         }
+
+    private static Pets createPet(String typeOfPet, String namesOfPet) {
+
+        return null;
+    }
 
 
 }
